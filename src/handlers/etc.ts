0fbfc497b65daa8f32b9b4370ec7411f2c997f91
Sreelal TS <heysreelal@gmail.com>
2022-02-24 19:27:50 +0530

    Profile Fix, and next messages

--- src/handlers/etc.ts
+++ src/handlers/etc.ts
@@ -1,13 +1,16 @@
 import { Context } from "grammy";
-import { errors } from "../config/strings";
+import { errors, excitedMessages } from "../config/strings";
 import { getFormatedDuration } from "../helpers/date";
-import { gameNo } from "../helpers/word_updater";
+import { random } from "../helpers/utils";
 import WordleDB from "../services/db";
 
 export async function nextWord(ctx: Context) {
     const game = WordleDB.getToday();
     ctx.replyWithChatAction("typing");
-    await ctx.reply(`New word showing up on ${getFormatedDuration(game.next)}!`);
+    ctx.replyWithChatAction("typing");
+    const msg = random(excitedMessages)
+        .replace('{TIME}', getFormatedDuration(game.next));
+    await ctx.reply(msg);
 }
 
 export async function profileHandler(ctx: Context) {
@@ -20,9 +23,9 @@ export async function profileHandler(ctx: Context) {
     ctx.replyWithChatAction("typing");
     await ctx.reply(`Hello <b>${ctx.from.first_name}</b>\n\n` +
         `🎰 Total Games Played: <b>${user.totalGamesPlayed}</b>\n\n` +
-        `🔥 Current Streak: <b>${user.streak}</b>\n\n` + 
+        `🔥 Current Streak: <b>${user.streak}</b>\n\n` +
         `🎆 Highest Streak: <b>${user.maxStreak}</b>\n\n` +
-        `💎 Win Percentage: <b>${(user.totalWins * 100 / gameNo()).toFixed(2)}</b>\n\n` +
+        `💎 Win Percentage: <b>${(user.totalWins * 100 / user.totalGamesPlayed).toFixed(2)}</b>\n\n` +
         `#MyWordle`, {
         parse_mode: "HTML"
     });
