b5fcbeb1e3bc4131d175475ed245a22929b4b91e
Sreelal TS <heysreelal@gmail.com>
2022-03-11 18:03:28 +0530

    Minor fix 🧑🏻‍💻

--- src/helpers/error_logger.ts
+++ src/helpers/error_logger.ts
@@ -2,5 +2,6 @@ import { Context } from "grammy";
 import { doLog } from "./utils";
 
 export default async function handleErrorWithEase(err:any, ctx: Context, where: string) {
-    await doLog(`Error on ${where} for ${ctx.from.id}:\n\n${err}`);
+    doLog(`Error in ${where}: ${err}`);
+    if(ctx && ctx.from) await doLog(`Error on ${where} for ${ctx.from.id}:\n\n${err}`);
 }
